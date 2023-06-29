a69fb2147e3bec689f81dd2e07ac0ba6d7e006c5
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2023-06-29 18:49:01 +0200

    [Sec2] finished-section-2

--- src/com/barosanu/Launcher.java
+++ src/com/barosanu/Launcher.java
@@ -1,5 +1,6 @@
 package com.barosanu;
 
+import com.barosanu.view.ViewFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,11 +17,7 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
-
-        Scene scene = new Scene(parent, 450, 250);
-        stage.setScene(scene);
-
-        stage.show();
+        ViewFactory viewFactory = new ViewFactory(new EmailManager());
+        viewFactory.showLoginWindow();
     }
 }
